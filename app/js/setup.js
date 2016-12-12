1e1a12978389640e6e0d34ecd30c84d827618720
xuchunyang <xuchunyang@192.168.3.173>
2016-12-12 15:25:43 +0800

    添加关于科袖，用户协议

--- app/js/setup.js
+++ app/js/setup.js
@@ -4,10 +4,36 @@ mui.ready(function() {
 	/*定义全局变量*/
 	var removeId = document.getElementById("removeid");
 	var security = document.getElementById("security");
+	var userAgreement = document.getElementById("userAgreement");
+	var about = document.getElementById("about");
 	
+	/*账户与安全*/
 	security.addEventListener('tap',function(){
 		plus.nativeUI.showWaiting();//显示原生等待框
         webviewShow = plus.webview.create("../html/security.html","../html/security.html");
+	});
+	
+	/*关于科袖*/
+	about.addEventListener('tap',function(){
+		mui.openWindow({
+			url: '../html/aboutus.html',
+			id: '../html/aboutus.html',
+			show: {
+				aniShow: "slide-in-right"
+			}
+		});
+		
+	});
+	
+	/*用户协议*/
+	userAgreement.addEventListener('tap',function(){
+		mui.openWindow({
+			url: '../html/privacy.html',
+			id: '../html/privacy.html',
+			show: {
+				aniShow: "slide-in-right"
+			}
+		});
 	});
 
 	/*退出按钮*/

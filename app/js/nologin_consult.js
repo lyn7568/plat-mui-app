2be1bdd638ce66cadc76d5029c4c0aacbf815a25
dell <dell@DESKTOP-3GQAT24>
2016-12-14 11:08:57 +0800

    添加未登录或退出时的咨询html,js

--- app/js/nologin_consult.js
+++ app/js/nologin_consult.js
@@ -1,5 +1,32 @@
 mui.plusReady(function() {
 	
+	var regBtn = document.getElementById("regBtn");
+	var logBtn = document.getElementById("logBtn");
+	
+//	注册
+	regBtn.addEventListener('tap',function(){
+		mui.openWindow({
+			url: '../html/reg.html',
+			id: '../html/reg.html',
+			show: {
+				aniShow: "slide-in-right"
+			}
+		});
+		
+	});
+	
+	//登陆
+	logBtn.addEventListener('tap',function(){
+		mui.openWindow({
+			url: '../html/login.html',
+			id: '../html/login.html',
+			show: {
+				aniShow: "slide-in-right"
+			}
+		});
+		
+	});
+	
 	var userid = plus.storage.getItem('userid'); 
 	
 	

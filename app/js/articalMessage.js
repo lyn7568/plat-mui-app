57b418ef18f1147cf940a0c67803a955dbb79239
jack <jack@192.168.3.46>
2017-01-23 15:19:48 +0800

    留言模块

--- app/js/articalMessage.js
+++ app/js/articalMessage.js
@@ -1,8 +1,8 @@
 mui.plusReady(function() {
 	var userid = plus.storage.getItem('userid');
 	var self = plus.webview.currentWebview();
-	var proId = self.articleId;	
-	console.log(proId);
+	var proId = self.articleId;
+
 	function proInfoMain() {
 		mui.ajax(baseUrl + "/ajax/leaveWord/ql", {
 			dataType: 'json', //数据格式类型
@@ -12,11 +12,43 @@ mui.plusReady(function() {
 			},
 			timeout: 10000, //超时设置
 			success: function(data) {
-				console.log(JSON.stringify(data))
 				var $info = data.data || {};
 				if(data.success && data.data) {
+					document.getElementsByClassName(" protable")[0].innerHTML = "";
 					plus.nativeUI.closeWaiting();
 					plus.webview.currentWebview().show("slide-in-right", 150);
+					var $info = data.data;
+					for(var i = 0; i < $info.length; i++) {
+						var time = $info[i].createTime.substring(0, 4) + "." + $info[i].createTime.substring(4, 6) + "." + $info[i].createTime.substring(6, 8) + " " + $info[i].createTime.substring(8, 10) + ":" + $info[i].createTime.substring(10, 12);
+						var li = document.createElement('li');
+						li.className = 'mui-table-view-cell mui-media';
+						var string = '<div class="proinfor">'
+						if($info[i].professor.hasHeadImage) {
+							string += '<img class="mui-media-object mui-pull-left headimg headRadius" src="' + baseUrl + '/images/head/' + $info[i].professor.id + '_l.jpg">'
+						} else {
+							string += '<img class="mui-media-object mui-pull-left headimg headRadius" src="../images/default-photo.jpg">'
+						}
+						string += '<div class="mui-media-body">'
+						string += '<div><span class="listtit">' + $info[i].professor.name
+						if($info[i].professor.authType) {
+							string += '<em class="mui-icon iconfont icon-vip authicon-cu"> </em>';
+						} else {
+							if($info[i].professor.authStatus) {
+								if($info[i].professor.authentication == 1) {
+									string += '<em class="mui-icon iconfont icon-renzheng authicon-mana"></em>';
+								} else if($info[i].professor.authentication == 2) {
+									string += '<em class="mui-icon iconfont icon-renzheng authicon-staff"></em>';
+								} else {
+									string += '<em class="mui-icon iconfont icon-renzheng authicon-stu"></em>';
+								}
+							}
+						}
+						string += '</span><span class="thistime timenow">' + time + '</span></div>'
+						string += '<p class="listtit3">' + $info[i].content + '</p>'
+						string += '</div> </div>'
+						li.innerHTML = string;
+						document.getElementsByClassName(" protable")[0].appendChild(li);
+					}
 				}
 			},
 			error: function() {
@@ -26,8 +58,47 @@ mui.plusReady(function() {
 		});
 	}
 	proInfoMain();
-	document.getElementById("send").addEventListener("tap",function(){
-		alert(1);
-		return;
-	})
+
+	function trim(str) { //删除左右两端的空格
+		　　
+		return str.replace(/(^\s*)|(\s*$)/g, "");　　
+	}
+
+	document.getElementById("msg-text").addEventListener("keyup", function() {
+		var valueLength = document.getElementById("msg-text").value;
+		var length = trim(valueLength);
+		if(length) {
+			document.getElementById("send").classList.add("add");
+			document.getElementById("msg-type").style.color = "red";
+		} else {
+			document.getElementById("send").classList.remove("add");
+		}
+	});
+	mui("#chatFooter").on('tap', '.add', function() {
+		article();
+	});
+
+	function article() {
+		mui.ajax(baseUrl + "/ajax/leaveWord", {
+			dataType: 'json', //数据格式类型
+			type: 'POST', //http请求类型
+			data: {
+				"articleId": proId,
+				"sender": userid,
+				"content": document.getElementsByTagName("textarea")[0].value,
+			},
+			timeout: 10000, //超时设置
+			success: function(data) {
+				var $info = data.data || {};
+				if(data.success && data.data) {
+					document.getElementsByTagName("textarea")[0].value = "";
+					proInfoMain();
+				}
+			},
+			error: function() {
+				plus.nativeUI.toast("服务器链接超时", toastStyle);
+				return;
+			}
+		});
+	}
 });
--- app/js/professorArticle.js
+++ app/js/professorArticle.js
@@ -1,7 +1,8 @@
 mui.plusReady(function() {
 	var userid = plus.storage.getItem('userid');
 	var self = plus.webview.currentWebview();
-	var proId = self.articleId;	
+	var proId = self.articleId;
+	console.log(userid)
 	function proInfoMain() {
 		mui.ajax(baseUrl + "/ajax/article/query", {
 			dataType: 'json', //数据格式类型
@@ -18,78 +19,90 @@ mui.plusReady(function() {
 					plus.webview.currentWebview().show("slide-in-right", 150);
 					var nameli = document.getElementById("nameli");
 					var proZlist = document.getElementById("proZlist"); //专家资源
+					var proRlist = document.getElementById("proRlist"); //专家资源
 					var proName = document.getElementById("proName");
 					var proTitle = document.getElementById("proTitle");
 					var proOffice = document.getElementById("proOffice");
 					var proOrg = document.getElementById("proOrg");
 					var proAddress = document.getElementById("proAddress");
-					var articleImg=document.getElementById("articleImg");
-					var artical_topic=document.getElementById("artical_topic");
-					var main_content=document.getElementById("main_content");
+					var articleImg = document.getElementById("articleImg");
+					var artical_topic = document.getElementById("artical_topic");
+					var main_content = document.getElementById("main_content");
 					proName.innerText = $info.professor.name;
-					if($info.professor.title&&$info.professor.office&&$info.professor.orgName&&$info.professor.address){
-						proTitle.innerText=$info.professor.title+"，";
-						proOffice.innerText=$info.professor.office+"，";
-						proOrg.innerText=$info.professor.orgName+" | ";
-						proAddress.innerText=$info.professor.address;
-					}else if(!$info.professor.title&&$info.professor.office&&$info.professor.orgName&&$info.professor.address){
-						proOffice.innerText=$info.professor.office+"，";
-						proOrg.innerText=$info.professor.orgName+" | ";
-						proAddress.innerText=$info.professor.address;
-					}else if($info.professor.title&&!$info.professor.office&&$info.professor.orgName&&$info.professor.address){
-						proTitle.innerText=$info.professor.title+"，";						
-						proOrg.innerText=$info.professor.orgName+" | ";
-						proAddress.innerText=$info.professor.address;
-					}else if($info.professor.title&&$info.professor.office&&!$info.professor.orgName&&$info.professor.address){
-						proTitle.innerText=$info.professor.title+"，";
-						proOffice.innerText=$info.professor.office+" | ";						
-						proAddress.innerText=$info.professor.address;
-					}else if($info.professor.title&&$info.professor.office&&$info.professor.orgName&&!$info.professor.address){
-						proTitle.innerText=$info.professor.title+"，";
-						proOffice.innerText=$info.professor.office+"，";
-						proOrg.innerText=$info.professor.orgName;						
-					}else if(!$info.professor.title&&!$info.professor.office&&$info.professor.orgName&&$info.professor.address){
-						proOrg.innerText=$info.professor.orgName+" | ";
-						proAddress.innerText=$info.professor.address;					
-					}else if(!$info.professor.title&&$info.professor.office&&!$info.professor.orgName&&$info.professor.address){						
-						proOffice.innerText=$info.professor.office+" | ";						
-						proAddress.innerText=$info.professor.address;					
-					}else if(!$info.professor.title&&$info.professor.office&&$info.professor.orgName&&!$info.professor.address){						
-						proOffice.innerText=$info.professor.office+"，";
-						proOrg.innerText=$info.professor.orgName;										
-					}else if($info.professor.title&&!$info.professor.office&&!$info.professor.orgName&&$info.professor.address){
-						proTitle.innerText=$info.professor.title+" | ";						
-						proAddress.innerText=$info.professor.address;					
-					}else if($info.professor.title&&!$info.professor.office&&$info.professor.orgName&&!$info.professor.address){						
-						proOffice.innerText=$info.professor.title+"，";						
-						proAddress.innerText=$info.professor.orgName;					
-					}else if($info.professor.title&&$info.professor.office&&!$info.professor.orgName&&!$info.professor.address){
-						proTitle.innerText=$info.professor.title+"，";
-						proOffice.innerText=$info.professor.office;					
-					}else if(!$info.professor.title&&!$info.professor.office&&!$info.professor.orgName&&$info.professor.address){
-						proAddress.innerText=$info.professor.address;					
-					}else if(!$info.professor.title&&!$info.professor.office&&$info.professor.orgName&&!$info.professor.address){
-						proOrg.innerText=$info.professor.orgName;					
-					}else if(!$info.professor.title&&$info.professor.office&&!$info.professor.orgName&&!$info.professor.address){
-						proOffice.innerText=$info.professor.office;	
-					}else if($info.professor.title&&!$info.professor.office&&!$info.professor.orgName&&!$info.professor.address){
-						proTitle.innerText=$info.professor.title;						
+					if($info.professor.title && $info.professor.office && $info.professor.orgName && $info.professor.address) {
+						proTitle.innerText = $info.professor.title + "，";
+						proOffice.innerText = $info.professor.office + "，";
+						proOrg.innerText = $info.professor.orgName + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if(!$info.professor.title && $info.professor.office && $info.professor.orgName && $info.professor.address) {
+						proOffice.innerText = $info.professor.office + "，";
+						proOrg.innerText = $info.professor.orgName + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if($info.professor.title && !$info.professor.office && $info.professor.orgName && $info.professor.address) {
+						proTitle.innerText = $info.professor.title + "，";
+						proOrg.innerText = $info.professor.orgName + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if($info.professor.title && $info.professor.office && !$info.professor.orgName && $info.professor.address) {
+						proTitle.innerText = $info.professor.title + "，";
+						proOffice.innerText = $info.professor.office + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if($info.professor.title && $info.professor.office && $info.professor.orgName && !$info.professor.address) {
+						proTitle.innerText = $info.professor.title + "，";
+						proOffice.innerText = $info.professor.office + "，";
+						proOrg.innerText = $info.professor.orgName;
+					} else if(!$info.professor.title && !$info.professor.office && $info.professor.orgName && $info.professor.address) {
+						proOrg.innerText = $info.professor.orgName + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if(!$info.professor.title && $info.professor.office && !$info.professor.orgName && $info.professor.address) {
+						proOffice.innerText = $info.professor.office + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if(!$info.professor.title && $info.professor.office && $info.professor.orgName && !$info.professor.address) {
+						proOffice.innerText = $info.professor.office + "，";
+						proOrg.innerText = $info.professor.orgName;
+					} else if($info.professor.title && !$info.professor.office && !$info.professor.orgName && $info.professor.address) {
+						proTitle.innerText = $info.professor.title + " | ";
+						proAddress.innerText = $info.professor.address;
+					} else if($info.professor.title && !$info.professor.office && $info.professor.orgName && !$info.professor.address) {
+						proOffice.innerText = $info.professor.title + "，";
+						proAddress.innerText = $info.professor.orgName;
+					} else if($info.professor.title && $info.professor.office && !$info.professor.orgName && !$info.professor.address) {
+						proTitle.innerText = $info.professor.title + "，";
+						proOffice.innerText = $info.professor.office;
+					} else if(!$info.professor.title && !$info.professor.office && !$info.professor.orgName && $info.professor.address) {
+						proAddress.innerText = $info.professor.address;
+					} else if(!$info.professor.title && !$info.professor.office && $info.professor.orgName && !$info.professor.address) {
+						proOrg.innerText = $info.professor.orgName;
+					} else if(!$info.professor.title && $info.professor.office && !$info.professor.orgName && !$info.professor.address) {
+						proOffice.innerText = $info.professor.office;
+					} else if($info.professor.title && !$info.professor.office && !$info.professor.orgName && !$info.professor.address) {
+						proTitle.innerText = $info.professor.title;
 					}
-					var zlist="";
+					var rlist = ''
+					for(var n = 0; n < $info.professor.researchAreas.length; n++) {
+						rlist += '<span>' + $info.professor.researchAreas[n].caption + '</span>';
+						if(n != $info.professor.researchAreas.length - 1) {
+							rlist += '，';
+						}
+					}
+					($info.professor.researchAreas) ? proRlist.innerHTML = rlist: proRlist.innerText = '';
+					var zlist = "";
 					for(var n = 0; n < $info.professor.resources.length; n++) {
 						zlist += '<span>' + $info.professor.resources[n].resourceName + '</span>';
-						if(n!=$info.professor.resources.length-1){
+						if(n != $info.professor.resources.length - 1) {
 							zlist += '，';
 						}
 					}
 					($info.professor.resources) ? proZlist.innerHTML = zlist: proZlist.innerText = '';
-					if($info.articleImg){
-						articleImg.style.backgroundImage='url('+baseUrl+'/data/article/'+$info.articleImg+')';
+					if($info.articleImg) {
+						articleImg.style.backgroundImage = 'url(' + baseUrl + '/data/article/' + $info.articleImg + ')';
 					}
-					artical_topic.innerText=$info.articleTitle;
-					if($info.articleContent){
-						main_content.innerHTML=$info.articleContent;
-						var oImg=main_content.getElementsByTagName("img");
+					if($info.professor.hasHeadImage) {
+						document.getElementById('proHead').src = baseUrl + "/images/head/" + item.id + "_l.jpg";
+					}
+					artical_topic.innerText = $info.articleTitle;
+					if($info.articleContent) {
+						main_content.innerHTML = $info.articleContent;
+						var oImg = main_content.getElementsByTagName("img");
 						for(var i = 0; i < oImg.length; i++) {
 							(function(n) {
 								var att = oImg[n].src.substr(7);
@@ -97,7 +110,7 @@ mui.plusReady(function() {
 							})(i);
 						}
 					}
-				} 
+				}
 			},
 			error: function() {
 				plus.nativeUI.toast("服务器链接超时", toastStyle);
@@ -107,8 +120,9 @@ mui.plusReady(function() {
 	}
 	proInfoMain();
 	/*进入留言*/
-	document.getElementById("leaveWord").addEventListener("tap",function(){
-		if(!userid){
+	document.getElementById("leaveWord").addEventListener("tap", function() {
+		var userid = plus.storage.getItem('userid');
+		if(!userid) {
 			goLoginFun();
 			return;
 		}

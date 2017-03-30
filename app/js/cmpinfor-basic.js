49ca94c4e0002c17dc298675ace3694611896da9
jack <jack@192.168.3.57>
2017-03-30 10:36:26 +0800

    企业浏览信息

--- app/js/cmpinfor-basic.js
+++ app/js/cmpinfor-basic.js
@@ -15,24 +15,29 @@ mui.ready(function() {
 				arr[2].show();
 			}
 		})
-	});
-	var id = "1B17DF270F0B4AAB8E1633267E4E2F5E";
+		var id = "1B17DF270F0B4AAB8E1633267E4E2F5E";
 
-	function companyMessage() {
-		mui.ajax(baseUrl + "/ajax/org/" + id, {
+		function companyMessage() {
+			mui.ajax(baseUrl + "/ajax/org/" + id, {
 				dataType: 'json', //数据格式类型
 				type: 'GET', //http请求类型
 				timeout: 10000, //超时设置
 				success: function(data) {
 					if(data.success) {
-						console.log(JSON.stringify(data));
+						//console.log(JSON.stringify(data));
 						var $data = data.data;
-						var orgType,orgSize;
+						var orgType, orgSize;
 						document.getElementById("companyName").innerText = $data.name;
 						if($data.hasOrgLogo) {
 							document.getElementById("oimg").src = baseUrl + "/images/org/" + $data.id + ".jpg";
 						} else {
-							document.getElementById("oimg").src = "../images/default-icon.jpg"
+							document.getElementById("oimg").src = "../images/default-icon.jpg";
+						}
+						/*企业标识*/
+						if($data.authStatus==3){
+							document.getElementById("authFlag").classList.add("authicon-com-ok");
+						}else{
+							document.getElementById("authFlag").classList.add("authicon-com-no");
 						}
 						/*企业类型*/
 						if($data.orgType) {
@@ -58,58 +63,103 @@ mui.ready(function() {
 							}
 							document.getElementById("orgType").innerText = orgType;
 							document.getElementById("comType").innerText = orgType;
-						}else{
+						} else {
 							document.getElementById("comType").parentNode.removeChild(document.getElementById("comType"));
 						}
 						/*所在城市*/
 						if($data.city) {
 							document.getElementById("ocity").innerText = $data.city;
-					} else {
-						document.getElementById("ocity").parentNode.removeChild(document.getElementById("ocity"));
-					}
-					/*时间*/
-					if($data.foundTime) {
-						var oTime = timeGeshi($data.foundTime);
-						document.getElementById("createTime").innerText=oTime;
-					} else {
-						document.getElementById("createTime").parentNode.style="none";
-					}
-					/*企业规模*/
-					if($data.orgSize) {
-						switch($data.orgSize) {
-							case '1':
-								orgSize.innerText="50人以内";
-								break;
-							case '2':
-								$("#qualificationList").text("50-100人")
-								break;
-							case '3':
-								$("#qualificationList").text("100-200人")
-								break;
-							case '4':
-								$("#qualificationList").text("200-500人")
-								break;
-							case '5':
-								$("#qualificationList").text("500-1000人")
-								break;
-							default:
-								$("#qualificationList").text("1000人以上")
-								break;
+							document.getElementById("city").innerText= $data.city;
+						} else {
+							document.getElementById("ocity").parentNode.removeChild(document.getElementById("ocity"));
+							document.getElementById("city").parentNode.style.display="none";
 						}
+						/*时间*/
+						if($data.foundTime) {
+							var oTime = timeGeshi($data.foundTime);
+							document.getElementById("createTime").innerText = oTime;
+						} else {
+							document.getElementById("createTime").parentNode.style = "none";
+						}
+						/*企业规模*/
+						if($data.orgSize) {
+							switch($data.orgSize) {
+								case '1':
+									orgSize = "50人以内";
+									break;
+								case '2':
+									orgSize = "50-100人";
+									break;
+								case '3':
+									orgSize = "100-200人";
+									break;
+								case '4':
+									orgSize = "200-500人";
+									break;
+								case '5':
+									orgSize = "500-1000人";
+									break;
+								default:
+									orgSize = "1000人以上";
+									break;
+							}
+							document.getElementById("orgSize").innerText = orgSize;
+						}
+					} else {
+						document.getElementById("orgSize").parentNode.style.display = "none";
 					}
+					/*企业官网*/
+					if($data.orgUrl) {
+						document.getElementById("shotAddress").innerText=$data.orgUrl;
+					} else {
+						document.getElementById("shotAddress").parentNode.style.display="block";
+					}
+					/*企业简介*/
+					if($data.descp) {
+						document.getElementById("breifinfo").innerText=$data.descp;
+					}else{
+						document.getElementById("breifinfolist").style.display='none';
+					}
+					/*应用行业*/
+					if($data.industry) {
+						indu($data.industry, 'industryShow')
+					}else{
+						document.getElementById("industry").style.display="none";
+					}
+					/*专注领域*/
+					if($data.subject) {
+						indu($data.subject, 'subjectShow')
+					}else{
+						document.getElementById("subject").style.display="none";
+					}
+					/*企业资质*/
+					if($data.qualification) {
+						indu($data.qualification, 'qiye')
+					}else{
+						document.getElementById("qiyelist").style.display="none";
+					}
+				},
+				error: function() {
+					plus.nativeUI.toast("服务器链接超时", toastStyle);
+					return;
 				}
-
-			},
-			error: function() {
-				plus.nativeUI.toast("服务器链接超时", toastStyle);
-				return;
-			}
-		});
-}
-	/*时间格式转换*/
-	function timeGeshi(otm) {
-		var otme = otm.substring(0, 4) + "年" + otm.substring(4, 6) + "月" + otm.substring(6, 8)+"日";
-		return otme;
+			});
+		}
+		/*时间格式转换*/
+		function timeGeshi(otm) {
+			var otme = otm.substring(0, 4) + "年" + otm.substring(4, 6) + "月" + otm.substring(6, 8) + "日";
+			return otme;
+		}
+		/*应用行业及领域及企业纸质*/
+	function indu(oString, oSelector) {
+		var arr = oString.split(",");
+		var oArr = new Array();
+		var i;
+		for(i in arr) {
+				oArr.push('<li>' + arr[i] + '</li>');
+		}
+		document.getElementById(oSelector).innerHTML=oArr.join("");
 	}
-companyMessage();
+		companyMessage();
+	});
 })

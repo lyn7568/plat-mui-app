2500886c4a5edc05b34b485f0ff61644e2908b28
luyanan <1271778060@qq.com>
2017-07-12 17:51:59 +0800

    新版个人用户以及修改

--- app/js/public/base.js
+++ app/js/public/base.js
@@ -1,7 +1,7 @@
 //公共文件
 mui.init();
 //var baseUrl = "http://www.ekexiu.com",
-var baseUrl = "http://192.168.3.233",
+var baseUrl = "http://192.168.3.233:81",
 //var baseUrl = "http:192.168.3.85:80",    
 	toastStyle = {
 		'verticalAlign': 'top',
@@ -183,16 +183,155 @@ function commenTime(startTime){
 }
 /*时间转换*/
 function TimeTr(dealtime) {
-	var myDate = new Date(); 
+	var myDate = new Date();
 	var s = dealtime;
 	var y = s.substr(0, 4);
 	var m = s.substr(4, 2);
 	var d = s.substr(6, 2);
 	var h = s.substr(8, 2);
 	var minute = s.substr(10, 2);
-	var formatTime = m.replace(/\b(0+)/gi, "") + "月" + d.replace(/\b(0+)/gi, "")+ "日 " + h + ":" + minute;
-	if(y != myDate.getFullYear()){
-		formatTime = y + "年" + m.replace(/\b(0+)/gi, "") + "月" + d.replace(/\b(0+)/gi, "")+ "日 " + h + ":" + minute;
+	var formatTime;
+	if(s.length <= 6) {
+		formatTime = y + "年" + m.replace(/\b(0+)/gi, "") + "月";
+	} else if(s.length > 6 && s.length <= 8) {
+		formatTime = y + "年" + m.replace(/\b(0+)/gi, "") + "月" + d.replace(/\b(0+)/gi, "") + "日 ";
+	} else {
+		formatTime = m.replace(/\b(0+)/gi, "") + "月" + d.replace(/\b(0+)/gi, "") + "日 " + h + ":" + minute;
+		if(y != myDate.getFullYear()) {
+			formatTime = y + "年" + m.replace(/\b(0+)/gi, "") + "月" + d.replace(/\b(0+)/gi, "") + "日 " + h + ":" + minute;
+		}
 	}
 	return formatTime;
+}
+//企业规模
+var orgSizeShow = {
+	'1': '50人以内',
+	'2': '50-100人',
+	'3': '100-200人',
+	'4': '200-500人',
+	'5': '500-1000人',
+	'6': '1000人以上'
+}
+//企业类型
+var orgTypeShow = {
+	"2": "上市企业",
+	"3": "外资企业",
+	"4": "合资企业",
+	"5": "独资企业",
+	"6": "个体经营",
+	"7": "政府机构",
+	"8": "公益组织",
+	"9": "协会学会",
+	"10": "新闻媒体",
+	"11": "教育机构",
+	"undefined":""
+}
+//学位
+var eduDegree = {
+	"1": "博士",
+	"2": "硕士",
+	"3": "学士",
+	"4": "大专",
+	"5": "其他"
+}
+var userid;
+/*判断是否收藏资源文章或者是否关注专家*/
+function ifcollectionAbout(watchObject,num) {
+	mui.ajax(baseUrl + '/ajax/watch/hasWatch', {
+		data: {
+			"professorId": userid,
+			"watchObject": watchObject
+		},
+		dataType: 'json', //数据格式类型
+		type: 'get', //http请求类型
+		timeout: 10000,
+		async: false,
+		success: function(data) {
+			if(data.success && data.data != null) {
+				if(num=="1" || num == "6"){//已关注专家
+					document.getElementById("ifAttend").classList.remove("icon-shoucang");
+					document.getElementById("ifAttend").classList.add("icon-yishoucang");
+				}else{//已收藏资源或文章
+					document.getElementById("ifCollect").classList.remove("icon-shoucang");
+					document.getElementById("ifCollect").classList.add("icon-yishoucang");
+				}
+			} else {
+				if(num=="1" || num == "6"){//关注专家
+					document.getElementById("ifAttend").classList.add("icon-shoucang");
+					document.getElementById("ifAttend").classList.remove("icon-yishoucang");
+				}else{//收藏资源或文章
+					document.getElementById("ifCollect").classList.add("icon-shoucang");
+					document.getElementById("ifCollect").classList.remove("icon-yishoucang");
+				}
+			}
+		},
+		error: function() {
+			plus.nativeUI.toast("服务器链接超时", toastStyle);
+		}
+	});
+}
+
+/*收藏资源、文章或者关注专家*/
+function collectionAbout(watchObject, num) {
+	mui.ajax(baseUrl + '/ajax/watch', {
+		data: {
+			"professorId": userid,
+			"watchObject": watchObject,
+			"watchType": num
+		},
+		dataType: 'json', //数据格式类型
+		type: 'POST', //http请求类型
+		timeout: 10000,
+		async: false,
+		success: function(data) {
+			if(data.success) {
+				if(num=="1" || num == "6"){//关注专家
+					document.getElementById("ifAttend").classList.remove("icon-shoucang");
+					document.getElementById("ifAttend").classList.add("icon-yishoucang");
+					plus.nativeUI.toast("关注成功", toastStyle);
+				}else{//收藏资源或文章
+					document.getElementById("ifCollect").classList.remove("icon-shoucang");
+					document.getElementById("ifCollect").classList.add("icon-yishoucang");
+					plus.nativeUI.toast("收藏成功", toastStyle);
+				}
+			}
+		},
+		error: function() {
+			plus.nativeUI.toast("服务器链接超时", toastStyle);
+		}
+	});
+}
+
+/*取消收藏资源、文章或者取消关注专家*/
+function cancelCollectionAbout(watchObject, num) {
+	mui.ajax({
+		url: baseUrl + '/ajax/watch/delete',
+		data: {
+			professorId: userid,
+			watchObject: watchObject
+		},
+		dataType: 'json', //数据格式类型
+		type: 'post', //http请求类型
+		timeout: 10000,
+		async: true,
+		success: function(data) {
+			console.log(data.success)
+			if(data.success) {
+				if(num=="1" || num == "6"){//关注专家
+					document.getElementById("ifAttend").classList.add("icon-shoucang");
+					document.getElementById("ifAttend").classList.remove("icon-yishoucang");
+					plus.nativeUI.toast("已取消关注", toastStyle);
+				}else{//收藏资源或文章
+					document.getElementById("ifCollect").classList.add("icon-shoucang");
+					document.getElementById("ifCollect").classList.remove("icon-yishoucang");
+					plus.nativeUI.toast("已取消收藏", toastStyle);
+				}
+				
+			}
+		},
+		error: function(data) {
+			plus.nativeUI.toast("服务器链接超时", toastStyle);
+		}
+	});
+
 }

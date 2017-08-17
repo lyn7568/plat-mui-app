bec446c3bebfde804e08665b532361b2aa9ea7a1
luyanan <luyanan@ekexiu.com>
2017-08-17 13:50:18 +0800

    检测新版本

--- app/js/index.js
+++ app/js/index.js
@@ -20,11 +20,16 @@ function checkVersion(){
 					timeout: 10000, //超时设置
 					async: false,
 					success: function(data) {
-						console.log(JSON.stringify(data));
+						alert(JSON.stringify(data));
+						alert(wgtVer)
 						if (data.version > wgtVer) {
-							var btn = ["确定更新", "取消更新"];
-							mui.confirm("检测到新版本，是否更新？", "提示", btn, function(e) {
+							var btn = ["立即更新", "取消更新"];
+							mui.confirm("有新版本发布了，赶快更新吧", "提示", btn, function(e) {
 								if(e.index == 0) {
+									if(mui.os.ios) {
+										plus.runtime.openURL('https://itunes.apple.com/cn/app/ke-xiu-da-jian-qi-ye-yu-zhuan/id1197110983?l=en&mt=8');
+										return;
+									}
 								try {
 									     plus.nativeUI.showWaiting("检测更新...");
 									     //var d="http://192.168.3.233/download/app1.0.6.apk";
